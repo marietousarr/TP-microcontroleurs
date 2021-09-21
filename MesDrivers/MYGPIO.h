33eaa3ec75a95fffd5069c353267785dde07ebd3
marietousarr <76689981+marietousarr@users.noreply.github.com>
2021-09-21 22:38:17 +0200

    Add files via upload

---
+++ MesDrivers/MYGPIO.h
@@ -0,0 +1,29 @@
+#ifndef MYGPIO_H
+#define MYGPIO_H
+#include "stm32f10x.h"
+
+typedef struct
+{
+GPIO_TypeDef * GPIO ;
+char GPIO_Pin ; //numero de 0 a 15
+char GPIO_Conf ; // voir ci dessous
+} MyGPIO_Struct_TypeDef ;
+
+
+#define In_Floating 0x4 // a completer
+#define In_PullDown 0x8 // a completer
+#define In_PullUp 0x8 // a completer
+#define In_Analog 0x0 // a completer
+#define Out_Ppull 0x2 // a completer
+#define Out_OD 0x6 // a completer
+#define AltOut_Ppull 0xA // a completer
+#define AltOut_OD 0xE // a completer
+
+
+void MyGPIO_Init ( MyGPIO_Struct_TypeDef * GPIOStructPtr ) ;
+int MyGPIO_Read(GPIO_TypeDef * GPIO , char GPIO_Pin ) ; // renvoie 0 ou autre chose d i f f e r e n t de 0
+void MyGPIO_Set(GPIO_TypeDef * GPIO , char GPIO_Pin ) ;
+void MyGPIO_Reset(GPIO_TypeDef * GPIO , char GPIO_Pin ) ;
+void MyGPIO_Toggle(GPIO_TypeDef * GPIO , char GPIO_Pin ) ;
+
+#endif
