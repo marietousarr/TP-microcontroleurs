8c5cc83a9ac396a71c08417fc7aab336c885b8c3
marietousarr <76689981+marietousarr@users.noreply.github.com>
2021-09-23 17:05:16 +0200

    Add files via upload

---
+++ Projet Timer/Source/principal.c
@@ -0,0 +1,36 @@
+//#include "stm32f10x.h"
+#include "MyTimer.h"
+
+MyTimer_Struct_TypeDef Timer;
+
+int main(void)
+{
+	
+RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	
+	// Test avec fonctions
+Timer.timer = TIM2;
+Timer.ARR = 9999;
+Timer.PSC = 1199;
+MyTimer_Base_Init (&Timer );
+	
+MyTimer_Base_Start(Timer);
+while(1) 
+	MyTimer_Base_Stop(Timer);
+
+//Test sans fonctions
+/*
+TIM2->ARR = 9999;
+TIM2->PSC = 5299;
+TIM2->CR1 = TIM2->CR1 | ( 1 << 0);
+	
+	while(1) {
+		if (TIM2->SR & TIM_SR_UIF) { // Scrutation de UIF
+    TIM2->SR = TIM2->SR & ~TIM_SR_UIF; // Remise à zéro de UIF
+		}
+	}
+
+*/
+	
+}
+
