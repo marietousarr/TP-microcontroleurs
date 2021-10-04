103b0ab143ba12222e2227d7ed3dd15bf89e3d54
marietousarr <marietou60@gmail.com>
2021-10-04 18:10:50 +0200

    prep adc

--- Projet ADC/Source/principal.c
+++ Projet ADC/Source/principal.c
@@ -2,69 +2,18 @@
 #include "MyTimer.h"
 #include "Driver_GPIO.h"
 
-MyTimer_Struct_TypeDef Timer;
-MyGPIO_Struct_TypeDef broche_led;
-
-void Callback(){
-	MyGPIO_Toggle(GPIOA, 5);
-}
 
 int main(void)
 {
 	
-RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
 RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
 
 RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4) ; //on a écrit 1 au 2e, 3e et 4e bit de APB2ENR
 	
-// Paramètres led
-broche_led.GPIO = GPIOA;
-broche_led.GPIO_Pin = 0x05;
+ 
 
-//config de la led en push pull
-broche_led.GPIO_Conf = Out_Ppull;
-
-MyGPIO_Init(&broche_led);
-	
-// Test avec fonctions
-Timer.timer = TIM1;
-	
-	// Pour une periode de 500 ms
-//Timer.ARR = 65454;
-//Timer.PSC = 550;
-	
-	// Pour une fréquence de 100kHz  PSC x ARR = 720
-	// 1/100kHz=      * PSC * ARR
-Timer.ARR = 720;
-Timer.PSC = 1;
-
-MyTimer_Base_Init (&Timer);
-	
-//MyTimer_ActiveIT(Timer.timer ,0, Callback);
-	
-MyTimer_Base_Start(Timer);
-
-PWMRatio(Timer.timer ,20,1);
-MyTimer_PWM(Timer.timer,1); 
-
-while(1) {
-	//MyTimer_Base_Stop(Timer);
-	}
-
-//Test sans fonctions
-/*
-TIM2->ARR = 65454;
-TIM2->PSC = 550;
-TIM2->CR1 = TIM2->CR1 | ( 1 << 0);
-	
-	while(1) {
-		if (TIM2->SR & TIM_SR_UIF) { // Scrutation de UIF
-    TIM2->SR = TIM2->SR & ~TIM_SR_UIF; // Remise à zéro de UIF
-		}
-	}
-*/
-
+while(1);
 }
-
